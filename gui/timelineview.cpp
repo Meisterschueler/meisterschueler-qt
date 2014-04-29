06d09229a66466900a35f1b1653f87b971584f12
Konstantin Gründger <kammermark@gmx.de>
2014-04-29 20:52:19 +0200

    Update TimelineView

--- gui/timelineview.cpp
+++ gui/timelineview.cpp
@@ -1,5 +1,61 @@
 #include "timelineview.h"
 
-TimelineView::TimelineView()
+#include <QGraphicsEllipseItem>
+#include <QGraphicsScene>
+#include <QMouseEvent>
+#include <QPointF>
+#include <QPropertyAnimation>
+
+TimelineView::TimelineView(QGraphicsView *parent) : QGraphicsView(parent) {
+    graphicsScene = new QGraphicsScene(this);
+    setScene(graphicsScene);
+    setSceneRect(this->rect());
+    setRenderHint( QPainter::Antialiasing );
+    setHorizontalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
+    setVerticalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
+
+    originItem = new MyItem();
+    originItem->setPos(0, 0);
+    originItem->pen().setWidth( 10 );
+    originItem->setBrush(QBrush(QColor(255,0,0)));
+
+    graphicsScene->addItem(originItem);
+
+    animation = new QPropertyAnimation(originItem, "pos");
+    animation->setDuration(10000000);
+    animation->setStartValue(QPointF(0, 0));
+    animation->setEndValue(QPointF(-1000000, 0));
+
+    animation->start();
+}
+
+TimelineView::~TimelineView()
 {
+}
+
+void TimelineView::showNoteOnEvent(NoteOnEvent noteOnEvent) {
+    double x = this->width() - 30.0;
+    double y = (1.0 - noteOnEvent.getNote()/128.0) * this->height();
+
+    QGraphicsEllipseItem *ellipseItem = new QGraphicsEllipseItem(x-originItem->pos().x()-10, y-10, 20, 20, originItem);
+    ellipseItem->setPen(QPen(Qt::red));
+    ellipseItem->setBrush(QBrush(QColor(255,0,0)));
+}
+
+void TimelineView::showNoteOffEvent(NoteOffEvent noteOffEvent) {
+
+}
+
+void TimelineView::resizeEvent(QResizeEvent *event) {
+    setSceneRect(this->rect());
+}
+
+void TimelineView::mousePressEvent(QMouseEvent *event) {
+    QGraphicsView::mousePressEvent(event);
+
+    QPointF pointF = mapFromParent(event->pos());
+
+    QGraphicsEllipseItem *ellipseItem = new QGraphicsEllipseItem(pointF.x()-originItem->pos().x()-10, pointF.y()-10, 20, 20, originItem);
+    ellipseItem->setPen(QPen(Qt::red));
+    ellipseItem->setBrush(QBrush(QColor(255,0,0)));
 }
--- gui/timelineview.h
+++ gui/timelineview.h
@@ -1,10 +1,42 @@
 #ifndef TIMELINEVIEW_H
 #define TIMELINEVIEW_H
 
-class TimelineView
+#include <QGraphicsView>
+#include <QList>
+#include <QMouseEvent>
+
+#include "events.h"
+#include "bubblegraphicsitem.h"
+
+class QPropertyAnimation;
+
+class MyItem : public QObject, public QGraphicsEllipseItem {
+    Q_OBJECT
+    Q_PROPERTY(QPointF pos READ pos WRITE setPos)
+};
+
+class TimelineView : public QGraphicsView
 {
+    Q_OBJECT
+
 public:
-    TimelineView();
+    explicit TimelineView(QGraphicsView *parent = 0);
+    ~TimelineView();
+
+private:
+    QGraphicsScene *graphicsScene;
+    QPropertyAnimation *animation;
+    MyItem *originItem;
+
+    QMap<MidiPair, BubbleGraphicsItem*> midiPairBubbleMap;
+
+public slots:
+    void showNoteOnEvent(NoteOnEvent noteOnEvent);
+    void showNoteOffEvent(NoteOffEvent noteOffEvent);
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+    void mousePressEvent(QMouseEvent *event);
 };
 
 #endif // TIMELINEVIEW_H
